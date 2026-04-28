42e54f1f1c6c6a8a5a700ca66707ebe084453163
Heayoon Cha <tyg4567@gmail.com>
2026-04-28 23:46:07 +0000

    edit fact_311_dog_complaints.sql

--- models/assignment_work/marts/311nyc_dog_complaints/fact_311_dog_complaints.sql
+++ models/assignment_work/marts/311nyc_dog_complaints/fact_311_dog_complaints.sql
@@ -1,3 +1,6 @@
+-- Fact table for 311 dog complaints
+-- Grain: one row per complaint
+
 WITH source AS (
   SELECT *
   FROM {{ ref('stg_311nyc_dog_complaints') }}
@@ -7,34 +10,44 @@ final AS (
   SELECT
     request_id AS unique_key,
 
+    -- Incident Address Key (simplified)
     {{ dbt_utils.generate_surrogate_key([
-      'incident_address',
-      'street_name',
-      'address_type',
-      'city',
-      'community_board',
-      'council_district'
+      'incident_address'
     ]) }} AS incident_address_key,
 
-    {{ dbt_utils.generate_surrogate_key(['latitude', 'longitude']) }} AS location_id,
+    -- Location Key
+    {{ dbt_utils.generate_surrogate_key([
+      'latitude',
+      'longitude'
+    ]) }} AS location_id,
 
+    -- Complaint Type Key
     {{ dbt_utils.generate_surrogate_key([
       'complaint_type',
       'descriptor',
-      'descriptor_2',
       'status'
     ]) }} AS complaint_type_key,
 
-    {{ dbt_utils.generate_surrogate_key(['agency_name']) }} AS agency_key,
+    -- Agency Key
+    {{ dbt_utils.generate_surrogate_key([
+      'agency_name'
+    ]) }} AS agency_key,
 
+    -- Zipcode Key
     {{ dbt_utils.generate_surrogate_key([
       'incident_zip',
       'borough'
     ]) }} AS zipcode_key,
 
-    {{ dbt_utils.generate_surrogate_key(['created_date']) }} AS created_date_key,
+    -- Date Key
+    {{ dbt_utils.generate_surrogate_key([
+      'created_date'
+    ]) }} AS created_date_key,
 
-    {{ dbt_utils.generate_surrogate_key(['location_type']) }} AS location_type_key,
+    -- Location Type (dummy)
+    {{ dbt_utils.generate_surrogate_key([
+      "'UNKNOWN'"
+    ]) }} AS location_type_key,
 
     latitude,
     longitude
