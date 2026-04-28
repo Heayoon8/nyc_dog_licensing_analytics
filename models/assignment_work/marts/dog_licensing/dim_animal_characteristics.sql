57d311a31cb30d85eb87fb3a8cecc4b2b6bab20a
Heayoon Cha <tyg4567@gmail.com>
2026-04-28 22:32:08 +0000

    create dim_animal_characteristics

---
+++ models/assignment_work/marts/dog_licensing/dim_animal_characteristics.sql
@@ -0,0 +1,20 @@
+WITH animal_characteristics AS (
+  SELECT DISTINCT
+    breed_name,
+    CAST(NULL AS STRING) AS size_category,
+    CAST(NULL AS STRING) AS temperament
+  FROM {{ ref('stg_nyc_dog_licensing') }}
+  WHERE breed_name IS NOT NULL
+),
+
+final AS (
+  SELECT
+    {{ dbt_utils.generate_surrogate_key(['breed_name', 'size_category', 'temperament']) }} AS animal_characteristics_key,
+    breed_name,
+    size_category,
+    temperament
+  FROM animal_characteristics
+)
+
+SELECT *
+FROM final
