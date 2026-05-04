2e94aaad3a38873a00d2c5350ea8eed0f9892700
Heayoon Cha <tyg4567@gmail.com>
2026-05-04 01:24:44 +0000

    edit dim_location type

--- models/assignment_work/marts/311nyc_dog_complaints/dim_location_type.sql
+++ models/assignment_work/marts/311nyc_dog_complaints/dim_location_type.sql
@@ -1,13 +1,15 @@
 WITH location_types AS (
-  SELECT 'UNKNOWN' AS location_type
+    SELECT DISTINCT
+        location_type
+    FROM {{ ref('stg_311nyc_dog_complaints') }}
+    WHERE location_type IS NOT NULL
 ),
 
 final AS (
-  SELECT
-    {{ dbt_utils.generate_surrogate_key(['location_type']) }} AS location_type_key,
-    location_type
-  FROM location_types
+    SELECT
+        {{ dbt_utils.generate_surrogate_key(['location_type']) }} AS location_type_key,
+        location_type
+    FROM location_types
 )
 
-SELECT *
-FROM final
+SELECT * FROM final
