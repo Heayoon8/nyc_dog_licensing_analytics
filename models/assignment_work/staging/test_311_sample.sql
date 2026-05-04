8a46da14be80e07ef0018b31b365b60f39649af0
Heayoon Cha <tyg4567@gmail.com>
2026-05-04 01:03:40 +0000

    Edit sources.yml

--- models/assignment_work/staging/test_311_sample.sql
+++
@@ -1,9 +0,0 @@
--- Quick test to verify source connection works
-
-SELECT
-  unique_key,
-  created_date,
-  complaint_type,
-  borough
-FROM {{ source('raw_311', 'source_311nyc_service_requests') }}
-LIMIT 10
