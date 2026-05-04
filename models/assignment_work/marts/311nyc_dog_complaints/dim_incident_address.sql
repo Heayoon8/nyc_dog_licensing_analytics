75d93e93f4769d15014d62edcdaa21cd9f9f8283
Heayoon Cha <tyg4567@gmail.com>
2026-05-04 01:20:52 +0000

    edit incident_address

--- models/assignment_work/marts/311nyc_dog_complaints/dim_incident_address.sql
+++ models/assignment_work/marts/311nyc_dog_complaints/dim_incident_address.sql
@@ -1,33 +1,32 @@
 WITH incident_addresses AS (
-  SELECT DISTINCT
-    incident_address,
-    CAST(NULL AS STRING) AS street_name,
-    CAST(NULL AS STRING) AS address_type,
-    CAST(NULL AS STRING) AS city,
-    CAST(NULL AS STRING) AS community_board,
-    CAST(NULL AS STRING) AS council_district
-  FROM {{ ref('stg_311nyc_dog_complaints') }}
-  WHERE incident_address IS NOT NULL
+    SELECT DISTINCT
+        incident_address,
+        street_name,
+        address_type,
+        city,
+        community_board,
+        council_district
+    FROM {{ ref('stg_311nyc_dog_complaints') }}
+    WHERE incident_address IS NOT NULL
 ),
 
 final AS (
-  SELECT
-    {{ dbt_utils.generate_surrogate_key([
-      'incident_address',
-      'street_name',
-      'address_type',
-      'city',
-      'community_board',
-      'council_district'
-    ]) }} AS incident_address_key,
-    incident_address,
-    street_name,
-    address_type,
-    city,
-    community_board,
-    council_district
-  FROM incident_addresses
+    SELECT
+        {{ dbt_utils.generate_surrogate_key([
+            'incident_address',
+            'street_name',
+            'address_type',
+            'city',
+            'community_board',
+            'council_district'
+        ]) }} AS incident_address_key,
+        incident_address,
+        street_name,
+        address_type,
+        city,
+        community_board,
+        council_district
+    FROM incident_addresses
 )
 
-SELECT *
-FROM final
+SELECT * FROM final
