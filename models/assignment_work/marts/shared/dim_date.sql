a2488c9ae1fbc6855931e4a3700a5e330e9850f4
Heayoon Cha <tyg4567@gmail.com>
2026-05-04 01:18:27 +0000

    edit dim_date

--- models/assignment_work/marts/shared/dim_date.sql
+++ models/assignment_work/marts/shared/dim_date.sql
@@ -1,35 +1,34 @@
 WITH all_dates AS (
+    SELECT DISTINCT CAST(created_date AS DATE) AS full_date
+    FROM {{ ref('stg_311nyc_dog_complaints') }}
+    WHERE created_date IS NOT NULL
 
-  SELECT DISTINCT CAST(created_date AS DATE) AS full_date
-  FROM {{ ref('stg_311nyc_dog_complaints') }}
-  WHERE created_date IS NOT NULL
+    UNION DISTINCT
 
-  UNION DISTINCT
+    SELECT DISTINCT license_issued_date AS full_date
+    FROM {{ ref('stg_nyc_dog_licensing') }}
+    WHERE license_issued_date IS NOT NULL
 
-  SELECT DISTINCT license_issued_date AS full_date
-  FROM {{ ref('stg_nyc_dog_licensing') }}
-  WHERE license_issued_date IS NOT NULL
+    UNION DISTINCT
 
-  UNION DISTINCT
-
-  SELECT DISTINCT license_expired_date AS full_date
-  FROM {{ ref('stg_nyc_dog_licensing') }}
-  WHERE license_expired_date IS NOT NULL
+    SELECT DISTINCT license_expired_date AS full_date
+    FROM {{ ref('stg_nyc_dog_licensing') }}
+    WHERE license_expired_date IS NOT NULL
 ),
 
 date_dimension AS (
-  SELECT
-    {{ dbt_utils.generate_surrogate_key(['full_date']) }} AS date_key,
-    full_date,
-    EXTRACT(YEAR FROM full_date) AS year,
-    EXTRACT(MONTH FROM full_date) AS month,
-    FORMAT_DATE('%B', full_date) AS month_name,
-    EXTRACT(DAY FROM full_date) AS day_of_month,
-    FORMAT_DATE('%A', full_date) AS day_of_week,
-    EXTRACT(DAYOFWEEK FROM full_date) IN (1, 7) AS is_weekend,
-    FALSE AS is_holiday
-  FROM all_dates
+    SELECT
+        {{ dbt_utils.generate_surrogate_key(['full_date']) }} AS date_key,
+        full_date,
+        EXTRACT(YEAR FROM full_date) AS year,
+        EXTRACT(MONTH FROM full_date) AS month,
+        FORMAT_DATE('%B', full_date) AS month_name,
+        EXTRACT(DAY FROM full_date) AS day_of_month,
+        FORMAT_DATE('%A', full_date) AS day_of_week,
+        EXTRACT(DAYOFWEEK FROM full_date) IN (1, 7) AS is_weekend,
+        FALSE AS is_holiday
+    FROM all_dates
+    WHERE full_date IS NOT NULL  
 )
 
-SELECT *
-FROM date_dimension
+SELECT * FROM date_dimension
