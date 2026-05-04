437bd27f1b497c74a6589c340660f395ba30a5d6
Heayoon Cha <tyg4567@gmail.com>
2026-05-04 01:23:39 +0000

    Edit dim_agency

--- models/assignment_work/marts/311nyc_dog_complaints/dim_agency.sql
+++ models/assignment_work/marts/311nyc_dog_complaints/dim_agency.sql
@@ -1,16 +1,17 @@
 WITH agencies AS (
-  SELECT DISTINCT
-    agency_name
-  FROM {{ ref('stg_311nyc_dog_complaints') }}
-  WHERE agency_name IS NOT NULL
+    SELECT DISTINCT
+        agency,
+        agency_name
+    FROM {{ ref('stg_311nyc_dog_complaints') }}
+    WHERE agency IS NOT NULL
 ),
 
 final AS (
-  SELECT
-    {{ dbt_utils.generate_surrogate_key(['agency_name']) }} AS agency_key,
-    agency_name
-  FROM agencies
+    SELECT
+        {{ dbt_utils.generate_surrogate_key(['agency']) }} AS agency_key,
+        agency,
+        agency_name
+    FROM agencies
 )
 
-SELECT *
-FROM final
+SELECT * FROM final
