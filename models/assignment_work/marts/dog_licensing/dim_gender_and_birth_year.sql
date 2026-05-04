5125b04f2b39e1f6f9c84a472924abcd19adaa78
Heayoon Cha <tyg4567@gmail.com>
2026-05-04 01:47:16 +0000

    edit dim gender and birth year

--- models/assignment_work/marts/dog_licensing/dim_gender_and_birth_year.sql
+++ models/assignment_work/marts/dog_licensing/dim_gender_and_birth_year.sql
@@ -1,19 +1,21 @@
 WITH gender_birth_year AS (
-  SELECT DISTINCT
-    animal_gender,
-    CAST(animal_birth_year AS STRING) AS animal_birth_year
-  FROM {{ ref('stg_nyc_dog_licensing') }}
-  WHERE animal_gender IS NOT NULL
-     OR animal_birth_year IS NOT NULL
+    SELECT DISTINCT
+        animal_gender,
+        CAST(animal_birth_year AS STRING) AS animal_birth_year
+    FROM {{ ref('stg_nyc_dog_licensing') }}
+    WHERE animal_gender IS NOT NULL
+      AND animal_birth_year IS NOT NULL  
 ),
 
 final AS (
-  SELECT
-    {{ dbt_utils.generate_surrogate_key(['animal_gender', 'animal_birth_year']) }} AS gender_birth_year_key,
-    animal_gender,
-    animal_birth_year
-  FROM gender_birth_year
+    SELECT
+        {{ dbt_utils.generate_surrogate_key([
+            'animal_gender',
+            'animal_birth_year'
+        ]) }} AS gender_birth_year_key,
+        animal_gender,
+        animal_birth_year
+    FROM gender_birth_year
 )
 
-SELECT *
-FROM final
+SELECT * FROM final
