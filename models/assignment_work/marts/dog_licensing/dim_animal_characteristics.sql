c4f720c5658e851d8e58c96d9c2e6623ffec0c14
Heayoon Cha <tyg4567@gmail.com>
2026-05-04 01:41:10 +0000

    edit dim_animal characteristics

--- models/assignment_work/marts/dog_licensing/dim_animal_characteristics.sql
+++ models/assignment_work/marts/dog_licensing/dim_animal_characteristics.sql
@@ -1,24 +1,23 @@
 WITH animal_characteristics AS (
-  SELECT DISTINCT
-    breed_name,
-    CAST(NULL AS STRING) AS size_category,
-    CAST(NULL AS STRING) AS temperament
-  FROM {{ ref('stg_nyc_dog_licensing') }}
-  WHERE breed_name IS NOT NULL
+    SELECT DISTINCT
+        breed_name,
+        size_category,
+        temperament
+    FROM {{ source('raw_dog', 'Dim_Animal_CHaracteristics') }}
+    WHERE breed_name IS NOT NULL
 ),
 
 final AS (
-  SELECT
-    {{ dbt_utils.generate_surrogate_key([
-      'breed_name',
-      'size_category',
-      'temperament'
-    ]) }} AS animal_characteristics_key,
-    breed_name,
-    size_category,
-    temperament
-  FROM animal_characteristics
+    SELECT
+        {{ dbt_utils.generate_surrogate_key([
+            'breed_name',
+            'size_category',
+            'temperament'
+        ]) }} AS animal_characteristics_key,
+        breed_name,
+        size_category,
+        temperament
+    FROM animal_characteristics
 )
 
-SELECT *
-FROM final
+SELECT * FROM final
