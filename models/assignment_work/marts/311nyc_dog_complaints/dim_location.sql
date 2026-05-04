798b25224ff22404da9ca97eb0529b88f5865244
Heayoon Cha <tyg4567@gmail.com>
2026-05-04 00:35:14 +0000

    edit again

--- models/assignment_work/marts/311nyc_dog_complaints/dim_location.sql
+++ models/assignment_work/marts/311nyc_dog_complaints/dim_location.sql
@@ -1,28 +1,16 @@
--- Zipcode dimension shared by both sources
-WITH all_locations AS (
-    -- Get locations from 311 requests
-    SELECT DISTINCT
-        borough,
-        incident_zip AS zip_code
-    FROM {{ ref('stg_311nyc_dog_complaints') }}
-    WHERE incident_zip IS NOT NULL
-
-    UNION DISTINCT
-
-    -- Get locations from dog licensing
-    SELECT DISTINCT
-        'Unknown' AS borough, -- Dog licensing data lacks a borough column
-        owner_zipcode AS zip_code
-    FROM {{ ref('stg_nyc_dog_licensing') }}
-    WHERE owner_zipcode IS NOT NULL
+WITH locations AS (
+  SELECT DISTINCT
+    borough,
+    zipcode
+FROM {{ ref('stg_311nyc_dog_complaints') }}
+WHERE borough IS NOT NULL
+AND zipcode IS NOT NULL
 ),
-
-zipcode_dimension AS (
-    SELECT
-        {{ dbt_utils.generate_surrogate_key(['borough', 'zip_code']) }} AS zipcode_key,
-        zip_code,
-        borough
-    FROM all_locations
+final AS (
+SELECT
+{{ dbt_utils.generate_surrogate_key(['borough', 'zipcode']) }} AS location_id,
+    ST_GEOGPOINT(borough, zipcode) AS location
+FROM locations
 )
-
-SELECT * FROM zipcode_dimension
+SELECT *
+FROM final
