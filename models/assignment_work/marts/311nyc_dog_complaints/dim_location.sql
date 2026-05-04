83f65d5d23dfc188903f5bc153594aa648ab2b62
Heayoon Cha <tyg4567@gmail.com>
2026-05-04 01:38:09 +0000

    edit dim_location

--- models/assignment_work/marts/311nyc_dog_complaints/dim_location.sql
+++ models/assignment_work/marts/311nyc_dog_complaints/dim_location.sql
@@ -1,16 +1,17 @@
 WITH locations AS (
-  SELECT DISTINCT
-    borough,
-    zipcode
-FROM {{ ref('stg_311nyc_dog_complaints') }}
-WHERE borough IS NOT NULL
-AND zipcode IS NOT NULL
+    SELECT DISTINCT
+        latitude,
+        longitude
+    FROM {{ ref('stg_311nyc_dog_complaints') }}
+    WHERE latitude IS NOT NULL
+      AND longitude IS NOT NULL
 ),
+
 final AS (
-SELECT
-{{ dbt_utils.generate_surrogate_key(['borough', 'zipcode']) }} AS location_id,
-    ST_GEOGPOINT(borough, zipcode) AS location
-FROM locations
+    SELECT
+        {{ dbt_utils.generate_surrogate_key(['latitude', 'longitude']) }} AS location_id,
+        ST_GEOGPOINT(longitude, latitude) AS location
+    FROM locations
 )
-SELECT *
-FROM final
+
+SELECT * FROM final
