f683f71a8cb431341c46239a97dc4a8e8f16c252
Heayoon Cha <tyg4567@gmail.com>
2026-05-12 17:53:31 +0000

    edit dim_zipcode sql

--- models/assignment_work/marts/shared/dim_zipcode.sql
+++ models/assignment_work/marts/shared/dim_zipcode.sql
@@ -1,16 +1,35 @@
-WITH all_zipcodes AS (
+WITH complaints_zip AS (
     SELECT DISTINCT
         borough,
         incident_zip AS zipcode
     FROM {{ ref('stg_311nyc_dog_complaints') }}
     WHERE incident_zip IS NOT NULL
-    UNION DISTINCT
+),
+
+licensing_zip AS (
     SELECT DISTINCT
-        'Unknown' AS borough,
-        zip_code AS zipcode        
+        zip_code AS zipcode
     FROM {{ ref('stg_nyc_dog_licensing') }}
     WHERE zip_code IS NOT NULL
 ),
+
+-- licensing zipcode에 311에서 borough 매핑
+all_zipcodes AS (
+    SELECT
+        lz.zipcode,
+        COALESCE(cz.borough, 'Unknown') AS borough
+    FROM licensing_zip AS lz
+    LEFT JOIN complaints_zip AS cz
+        ON lz.zipcode = cz.zipcode
+
+    UNION DISTINCT
+
+    SELECT
+        zipcode,
+        borough
+    FROM complaints_zip
+),
+
 zipcode_dimension AS (
     SELECT
         {{ dbt_utils.generate_surrogate_key(['borough', 'zipcode']) }} AS zipcode_key,
@@ -18,4 +37,5 @@ zipcode_dimension AS (
         borough
     FROM all_zipcodes
 )
+
 SELECT * FROM zipcode_dimension
