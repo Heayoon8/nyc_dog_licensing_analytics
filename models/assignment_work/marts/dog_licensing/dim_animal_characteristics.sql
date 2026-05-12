a04d6999e6888b6ca2a5be8fff22c343e0888d8d
Heayoon Cha <tyg4567@gmail.com>
2026-05-12 17:17:43 +0000

    edit dim_animal_characteristics sql

--- models/assignment_work/marts/dog_licensing/dim_animal_characteristics.sql
+++ models/assignment_work/marts/dog_licensing/dim_animal_characteristics.sql
@@ -9,23 +9,19 @@ WITH animal_characteristics AS (
 
 dog_licensing AS (
     SELECT DISTINCT
-        breed_rc AS breed_name,       
-        animal_gender,
-        animal_birth_year
+        breed_name      
     FROM {{ ref('stg_nyc_dog_licensing') }}
-    WHERE breed_rc IS NOT NULL
+    WHERE breed_name IS NOT NULL
 ),
 
 
 joined AS (
     SELECT
-        ac.breed_name,
+        COALESCE(ac.breed_name, dl.breed_name) AS breed_name,
         ac.size_category,
-        ac.temperament,
-        dl.animal_gender,             
-        dl.animal_birth_year         
+        ac.temperament
     FROM animal_characteristics AS ac
-    LEFT JOIN dog_licensing AS dl
+    FULL OUTER JOIN dog_licensing AS dl
         ON ac.breed_name = dl.breed_name
 ),
 
@@ -38,9 +34,7 @@ final AS (
         ]) }} AS animal_characteristics_key,
         breed_name,
         size_category,
-        temperament,
-        animal_gender,
-        animal_birth_year
+        temperament
     FROM joined
 )
 
